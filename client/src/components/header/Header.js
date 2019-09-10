e96ddbcf51bba3c0475b8c1726611953a1632f93
BlueBoi904 <cliff.mirschel@gmail.com>
2019-09-10 11:03:47 -0400

    Added basic header image

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,11 +1,12 @@
 
 import React from 'react'
+import logo from '../../assets/logo.png'
 
 export default function Header() {
     return (
         <div>
             <header>
-                
+                <img  src={logo} alt='Logo'/>
             </header>
         </div>
     )
