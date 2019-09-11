3fb99bf063ec2e2b981ad4d60fa55f01c8f5ab75
Benji Mirschel <cliff.mirschel@gmail.com>
2019-09-10 21:35:02 -0400

    Added eslint features

--- routes/profile.js
+++ routes/profile.js
@@ -1,36 +1,37 @@
-const express = require("express");
-const router = express.Router();
-const fetch = require("node-fetch");
+const express = require('express');
 
-router.get("/:platform/:gamertag", async (req, res) => {
+const router = express.Router();
+
+const fetch = require('node-fetch');
+
+router.get('/:platform/:gamertag', async (req, res) => {
   try {
     const headers = {
-      "TRN-Api-Key": process.env.TRACKER_API_KEY
+      'TRN-Api-Key': process.env.TRACKER_API_KEY,
     };
-    //Destruct off the req.params obj
+    // Destruct off the req.params obj
     const { platform, gamertag } = req.params;
 
     const response = await fetch(
       `${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`,
       {
-        headers
-      }
+        headers,
+      },
     );
 
     const data = await response.json();
 
-    //Check if player does not exist, send our own custom response
+    // Check if player does not exist, send our own custom response
     if (data.errors && data.errors.length > 0) {
       return res.status(404).json({
-        message: "Profile not found"
+        message: 'Profile not found',
       });
     }
 
     res.json(data);
   } catch (err) {
-    console.error(err);
     res.status(500).json({
-      message: "Server Error"
+      message: 'Server Error',
     });
   }
 });
--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const morgan = require("morgan");
-const dotenv = require("dotenv");
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
 
 // Load env
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: './config.env' });
 
 const app = express();
 
 // Dev logging
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 // Profile routes
-app.use("/api/v1/profile", require("./routes/profile"));
+app.use('/api/v1/profile', require('./routes/profile'));
 
 const port = process.env.PORT || 8000;
 
